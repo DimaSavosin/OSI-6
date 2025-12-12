20733d3c00313dee7d764776477b6700ba1d6600
DimaSavosin <143866241+DimaSavosin@users.noreply.github.com>
2025-12-12 11:27:24 +0300

    task2.c

--- task2.c
+++ task2.c
@@ -12,11 +12,9 @@ double compute_term(int k, double x) {
     double fact = 1.0;
     for (int j = 2; j <= p; j++)
         fact *= j;
-
     double power = 1.0;
     for (int j = 0; j < p; j++)
         power *= x;
-
     double sign = (k % 2 == 0) ? 1.0 : -1.0;
 
     return sign * power / fact;
@@ -43,33 +41,24 @@ int main() {
     for (int i = 0; i < K; i++) {
         double x = 2.0 * PI * i / N;
         double sum = 0.0;
-
         for (int k = 0; k < n; k++) {
-
             int fd[2];
             pipe(fd);
-
             pid_t pid = fork();
-
             if (pid == 0) {
                 close(fd[0]);
-
                 double term = compute_term(k, x);
-
                 printf("Child process: pid=%d, ppid=%d, i=%d, term=%d, value=%lf\n",
                        getpid(), getppid(), i, k, term);
-
                 write(fd[1], &term, sizeof(double));
                 close(fd[1]);
                 _exit(0);
             }
             else if (pid > 0) {
                 close(fd[1]);
-
                 double term_value;
                 read(fd[0], &term_value, sizeof(double));
                 close(fd[0]);
-
                 waitpid(pid, NULL, 0);
                 sum += term_value;
             }
@@ -83,7 +72,6 @@ int main() {
     }
 
     fclose(f);
-
     printf("Results written to results.txt\n");
     return 0;
 }
